221f8b14fe9af2a478eef7bea5582f8ed2873dd5
Monika H J <monikahj7@gmail.com>
2025-04-04 13:26:08 +0530

    makoing main pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
+import Main from "./components/Main"
 
 export default function Home() {
   return (
-   <div className="">
+   <div className=" py-1.5 px-5  lg:px-5 md:px-1.5  sm:px-2">
      <Navbar/>
-    <p className="">Kushi Styles</p>
+
+     <div className="mt-3">
+     <Main/>
+     </div>
     
      <Footer/>
    </div>
