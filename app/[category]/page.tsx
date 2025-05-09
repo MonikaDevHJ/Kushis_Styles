907141b42326991a50b4cef8cf162b1c528a1caf
Monika H J <monikahj7@gmail.com>
2025-05-09 13:51:23 +0530

    womens page makign

--- app/[category]/page.tsx
+++ app/[category]/page.tsx
@@ -1,18 +1,41 @@
+import { it } from "node:test";
+import { Product } from "../Data/Product";
 
-interface Props {
-  params :{
-    category : string;
-  }
+interface props {
+  params: {
+    category: String;
+  };
 }
 
-const CategoryPage = ({params}:Props) =>{
-  const {category} = params;
- return(
-  <div>
-    <p>{category} Page</p>
-    <p>Show content for : {category}</p>
-  </div>
- )
 
+const CategoryPage = ({ params }: props) => {
+
+  const { category } = params;
+
+  const filterProduct = Product.filter(
+    (item) => item.categoryPage.toLowerCase() === category.toLowerCase()
+  );
+
+
+  return (
+    <div className="p-4">
+      <p className="">{category} Page</p>
+
+      {filterProduct.length === 0 ? (
+        <p>No Product  Found  for "{category}"</p>
+      ) : (
+        < div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-4">
+          {filterProduct.map((item)=>
+          <div>
+            
+          </div>
+           )}
+
+
+        </div>
+      )}
+    </div >
+
+  )
 }
 export default CategoryPage;
