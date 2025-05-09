bb9d3d87077db2f39f354b8ce2b1bec31fa088fa
Monika H J <monikahj7@gmail.com>
2025-05-09 14:52:50 +0530

    makign

--- app/[category]/page.tsx
+++ app/[category]/page.tsx
@@ -25,8 +25,9 @@ const CategoryPage = ({ params }: props) => {
         <p>No Product  Found  for "{category}"</p>
       ) : (
         < div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-4">
-          {filterProduct.map((item)=>
-          <div>
+          {filterProduct.map((item, index)=>
+          <div key={index} className="border p-4 rounded shadow ">
+            <p>{item.price}</p>
             
           </div>
            )}
