14d78e963501c7f6b736d630b5ac5dfae5677353
Monika H J <monikahj7@gmail.com>
2025-05-12 16:30:37 +0530

    working based on Filter

--- app/Data/Product.ts
+++ app/Data/Product.ts
@@ -1,10 +1,25 @@
-
-
 export const Product = [
-    {
-        id:1,image:"/assets/Kurta1.png",title:"Drees", price:299,categoryPage:"womens"
+  {
+    id: 1,
+    image: "/assets/Kurta1.png",
+    title: "Drees",
+    price: 299,
+    categoryPage: "womens"
+  },
+  {
+    id: 2,
+    image: "/assets/Kurta2.png",
+    title: "Drees",
+    price: 299,
+    categoryPage: "womens"
+  },
+  {
+    id: 3,
+    image: "/assets/ShirtM1.png",
+    title: "Drees",
+    price: 299,
+    categoryPage: "men"
+  }
 
-    }
-]
-
-
+ 
+];
